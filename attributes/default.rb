d1ce39bb8c53362e67eecf17051b2a8c22abf5ec
Léonard TAVAE <leonard.tavae@informatique.gov.pf>
2014-12-21 14:49:40 -1000

    Ajout du support de la VIP.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,6 @@
 
+# Configuration de la message queue (RABBITMQ)
+#
 default['dsi-mq']['admin'] = 'admin'
 default['dsi-mq']['admin_password'] = 'password'
 default['dsi-mq']['user'] = 'rabbitmq'
@@ -7,3 +9,9 @@ default['dsi-mq']['user_password'] = 'password'
 default['dsi-mq']['cluster'] = true
 default['dsi-mq']['nodes'] = []
 default['dsi-mq']['cookie'] = 'random_string_password'
+
+# Configuration de la VIP (KEEPALIVED)
+#
+default['dsi-mq']['vip'] = '172.28.128.5'
+default['dsi-mq']['bind'] = 'eth1'
+default['dsi-mq']['keypass'] = 'secret'
